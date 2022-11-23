61e8eb555cff3f37f8a3e63f7a2d81f7231e343b
w44t3r <vk.educate@protonmail.ch>
2022-11-23 04:33:20 +0100

    added smoothScroll for header-nav links

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
 (function () {
     const header = document.querySelector('header');
     window.onscroll = () => {
-        if (window.pageYOffset > 50) {
+        if (window.scrollY > 50) {
             header.classList.add('header-scroll');
 
         } else {
@@ -23,4 +23,43 @@
     menuClose.addEventListener('click', () => {
         menu.classList.remove('header-nav-active')
     });
+}());
+
+(function () {
+
+    const smoothScroll = function (targetEl, duration) {
+        const headerElHeight =  document.querySelector('header').clientHeight;
+        let target = document.querySelector(targetEl);
+        let targetPosition = target.getBoundingClientRect().top - headerElHeight;
+        let startPosition = window.scrollY;
+        let startTime = null;
+
+        const ease = function(t,b,c,d) {
+            t /= d / 2;
+            if (t < 1) return c / 2 * t * t + b;
+            t--;
+            return -c / 2 * (t * (t - 2) - 1) + b;
+        };
+
+        const animation = function(currentTime){
+            if (startTime === null) startTime = currentTime;
+            const timeElapsed = currentTime - startTime;
+            const run = ease(timeElapsed, startPosition, targetPosition, duration);
+            window.scrollTo(0,run);
+            if (timeElapsed < duration) requestAnimationFrame(animation);
+        };
+        requestAnimationFrame(animation);
+
+    };
+
+    const scrollTo = function () {
+        const links = document.querySelectorAll('.js-scroll');
+        links.forEach(each => {
+            each.addEventListener('click', function () {
+                const currentTarget = this.getAttribute('href');
+                smoothScroll(currentTarget, 1000);
+            });
+        });
+    };
+    scrollTo();
 }());
