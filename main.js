06269c18f4f3c5084ce4595c5e1af25554ae12fb
w44t3r <vk.educate@protonmail.ch>
2022-11-23 01:04:35 +0100

    added function for burger menu

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 // samo vizivaywaysa funktion
+/* header scroll */
 (function () {
     const header = document.querySelector('header');
     window.onscroll = () => {
@@ -8,7 +9,18 @@
         } else {
             header.classList.remove('header-scroll');
         }
-            };
+    };
+}());
 
-
+//burger
+(function () {
+    const burgerItem = document.querySelector('.burger');
+    const menu = document.querySelector('.header-nav')
+    const menuClose = document.querySelector('.header-nav-close')
+    burgerItem.addEventListener('click', () =>  {
+        menu.classList.add('header-nav-active')
+    });
+    menuClose.addEventListener('click', () => {
+        menu.classList.remove('header-nav-active')
+    });
 }());
